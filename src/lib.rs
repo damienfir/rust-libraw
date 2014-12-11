1d56aa95e30bb276cfe2ca4aa7f0e0c6a56441c1
Damien Firmenich <fir.damien@gmail.com>
2014-12-11 13:30:04 +0100

    ignoring vim swap files

--- src/lib.rs
+++ src/lib.rs
@@ -23,16 +23,16 @@ impl Image {
     pub fn load(&self) -> bool {
         let c_path = self.path.as_slice().to_c_str();
         unsafe {
-            ffi::libraw_open_file(self.data, c_path.as_ptr()) != 0 &&
-            ffi::libraw_unpack(self.data) != 0
+            ffi::libraw_open_file(self.data, c_path.as_ptr()) == 0 &&
+            ffi::libraw_unpack(self.data) == 0
         }
     }
 
     pub fn write(&self, path: &str) -> bool {
         let c_out = path.to_c_str();
         unsafe {
-            ffi::libraw_raw2image(self.data) != 0 &&
-            ffi::libraw_dcraw_ppm_tiff_writer(self.data, c_out.as_ptr()) != 0
+            ffi::libraw_raw2image(self.data) == 0 &&
+            ffi::libraw_dcraw_ppm_tiff_writer(self.data, c_out.as_ptr()) == 0
         }
     }
 }
